8316741920d160793abbdef844aeafcda7b75baa
Pablo Soriano <soriano.pablo@gmail.com>
2023-03-14 14:39:27 -0600

    Se crea el formulario para editar y crear nuevo producto, subiendo archivos con React

--- src/componentes/productos/EditarProducto.js
+++ src/componentes/productos/EditarProducto.js
@@ -1,8 +1,127 @@
-import React from 'react'
+import React, {Fragment, useState, useEffect } from 'react'
+import Swal from 'sweetalert2';
+import { clienteAxios } from '../../config/axios';
+import { useNavigate, useParams } from 'react-router-dom';
+import Spinner from '../layout/Spinner';
 
 function EditarProductos() {
+    const history = useNavigate(); 
+
+    // obtener el Id del producto
+    const {id} = useParams();
+
+    // producto = state, guardarProducto para actualizar
+    const [producto, guardarProducto] = useState({
+        nombre: '',
+        precio: '',
+        imagen: ''
+    });
+
+    // archivo = state, guardarArchivo = setState
+    const [archivo, guardarArchivo] = useState('');
+
+     // Consultar la API para traer el producto a editar
+     const consultarAPI = async () => {
+        const productoConsulta = await clienteAxios.get(`/productos/${id}`);
+        guardarProducto(productoConsulta.data);
+     }
+
+     // cuando el componente carga
+     useEffect( () => {
+        consultarAPI();
+     }, []);
+
+
+    // Edita un producto en BD
+    const editarProducto = async e => {
+        e.preventDefault();
+
+         // Crear un formdata (para el envio de imagenes a la API)
+         const formData = new FormData();
+         formData.append('nombre', producto.nombre);
+         formData.append('precio', producto.precio);
+         formData.append('imagen', archivo);
+ 
+         // almacenarlo en BD
+         try {
+            const res = await clienteAxios.put(`/productos/${id}`, formData, {
+                 headers: {
+                     'Content-Type': 'multipart/form-data'
+                 }
+             });
+ 
+             // lanzar una alerta
+             if(res.status === 200) {
+                 Swal.fire(
+                     'Editado Correctamente',
+                     res.data.mensaje,
+                     'success'
+                 )
+             }
+ 
+             //redirecccionar
+             history('/productos');
+ 
+         } catch (error) {
+             console.log(error);
+             // lanzar alerta
+             Swal.fire({
+                 type: 'error',
+                 title: 'Hubo un error',
+                 text: 'Vuelva a intentarlo'
+             })
+         }
+    }
+    
+    
+
+    // leer los datos del formulario
+    const leerInformacionProducto = e => {
+        guardarProducto({
+            //obtener una copia del state y agregar el nuevo
+            ...producto,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    //coloca la imagen en el state
+    const leerArchivo = e => {
+        guardarArchivo(e.target.files[0]);
+    }
+
+    // extraer los valores del state
+    const { nombre, precio, imagen } = producto;
+
+    if(!nombre) return <Spinner />
+
     return(
-        <h2>Editar Productos</h2>
+        <Fragment>
+            <h2>Editar Producto</h2>
+
+            <form onSubmit={editarProducto}>
+                <legend>Llena todos los campos</legend>
+
+                <div className="campo">
+                    <label>Nombre:</label>
+                    <input type="text" placeholder="Nombre Producto" name="nombre" onChange={leerInformacionProducto} defaultValue={nombre} />
+                </div>
+
+                <div className="campo">
+                    <label>Precio:</label>
+                    <input type="number" name="precio" min="0.00" step="0.01" placeholder="Precio" onChange={leerInformacionProducto} defaultValue={precio} />
+                </div>
+
+                <div className="campo">
+                    <label>Imagen:</label>
+                    { imagen ? ( <img src={`http://localhost:5000/${imagen}`} alt="imagen" width="300" />) : null }
+                    <input type="file"  name="imagen" onChange={leerArchivo}/>
+                </div>
+
+                <div className="enviar">
+                        <input type="submit" className="btn btn-azul" value="Editar Producto" />
+                </div>
+            </form>
+        </Fragment>
     )
 }
 
--- src/componentes/productos/NuevoProducto.js
+++ src/componentes/productos/NuevoProducto.js
@@ -1,6 +1,11 @@
 import React, {Fragment, useState} from 'react'
+import Swal from 'sweetalert2';
+import { clienteAxios } from '../../config/axios';
+import { useNavigate } from 'react-router-dom';
+
 
 function NuevoProducto() {
+    const history = useNavigate();
 
     // producto = state, guardarProducto = setState
     const [producto, guardarProducto] = useState({
@@ -10,6 +15,48 @@ function NuevoProducto() {
 
     // archivo = state, guardarArchivo = setState
     const [archivo, guardarArchivo] = useState('');
+
+    // Almacena nuevo producto en la BD
+    const agregarProducto = async e => {
+        e.preventDefault();
+
+        // Crear un formdata (para el envio de imagenes a la API)
+        const formData = new FormData();
+
+        formData.append('nombre', producto.nombre);
+        formData.append('precio', producto.precio);
+        formData.append('imagen', archivo);
+
+        // almacenarlo en BD
+        try {
+           const res = await clienteAxios.post('/productos', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+
+            // lanzar una alerta
+            if(res.status === 200) {
+                Swal.fire(
+                    'Agregado Correctamente',
+                    res.data.mensaje,
+                    'success'
+                )
+            }
+
+            //redirecccionar
+            history('/productos');
+
+        } catch (error) {
+            console.log(error);
+            // lanzar alerta
+            Swal.fire({
+                type: 'error',
+                title: 'Hubo un error',
+                text: 'Vuelva a intentarlo'
+            })
+        }
+    }
 
     // leer los datos del formulario
     const leerInformacionProducto = e => {
@@ -29,7 +76,7 @@ function NuevoProducto() {
         <Fragment>
             <h2>Nuevo Producto</h2>
 
-            <form>
+            <form onSubmit={agregarProducto}>
                 <legend>Llena todos los campos</legend>
 
                 <div className="campo">
